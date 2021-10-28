f8e80135a9a75c86acd13a9bc8d0efe55fbf0ed0
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-28 10:09:30 +0200

    Agregadas las peticiones por teclado y el muestre del resultado por pantalla.

--- Ejercicio_7_sharp/Program.cs
+++ Ejercicio_7_sharp/Program.cs
@@ -6,7 +6,14 @@ namespace Ejercicio_7_sharp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Introduzca una palabra: ");
+            string palabra1 = Console.ReadLine();
+            Console.Write("Introduzca otra palabra: ");
+            string palabra2 = Console.ReadLine();
+
+            string resultado = palabra1 + " - " + palabra2;
+
+            Console.WriteLine("\n" + resultado);
         }
     }
 }
